1b935e59c86776e8b3c92cd08af57b43175c4c8a
dhvaniagola <dhvaniagola@dhvanis-mbp>
2019-02-13 18:57:03 -0500

    commit

---
+++ HackerRank/src/com/Algorithms/GraphTheory/extra/MinCut.java
@@ -0,0 +1,235 @@
+package com.Algorithms.GraphTheory.extra;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class MinCut {
+	static class graph {
+		LinkedList<Integer> ll[];
+
+		graph(int n) {
+			ll = new LinkedList[n];
+			for (int i = 0; i < n; i++)
+				ll[i] = new LinkedList<>();
+		}
+	}
+
+	private static void addEdge(graph g, int[][] edges) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < edges.length; i++)
+			g.ll[edges[i][0]].add(edges[i][1]);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 6;
+//		int edges[][] = { { 0, 1, 16 }, { 0, 2, 13 }, { 1, 3, 12 }, { 2, 1, 4 }, { 2, 4, 14 }, { 3, 2, 9 }, { 4, 3, 7 },
+//				{ 3, 5, 20 }, { 4, 5, 4 } };
+		int edges[][] = { { 0, 1, 5 }, { 0, 2, 5 }, { 1, 4, 6 }, { 1, 3, 3 }, { 2, 4, 3 }, { 2, 3, 1 }, { 3, 5, 6 },
+				{ 4, 5, 6 } };
+		graph g = new graph(n);
+		addEdge(g, edges);
+		int max = preTraverseGraph(g, n, edges);
+		System.out.println("max flow : " + max);
+	}
+
+	static List<List<Integer>> Flst = new ArrayList<>();
+	static int arr[][];
+	static List<Integer> ConnectivityLst = new ArrayList<>();
+
+	private static int preTraverseGraph(graph g, int n, int[][] edges) {
+		// TODO Auto-generated method stub
+
+		List<Integer> lst = new ArrayList<>();
+
+		while (true) {
+			boolean visited[] = new boolean[n];
+			Flst.clear();
+			lst.clear();
+
+			traverse(g, edges, lst, visited, 0, 5);
+			edges = arr;
+//			System.out.println("Selected Path : " + Flst);
+//			System.out.println("Recidual graph : ");
+//			for (int i = 0; i < edges.length; i++) {
+//				System.out.println(edges[i][0] + ", " + edges[i][1] + ", " + edges[i][2]);
+//			}
+//			System.out.println("edges : ");
+//			for (int i = 0; i < g.ll.length; i++) {
+//				System.out.println(i + " -> " + g.ll[i]);
+//			}
+			if (Flst.isEmpty()) {
+				break;
+			}
+//			System.out.println("----------------------------");
+		}
+		int max = 0;
+		for (int i = 0; i < edges.length; i++) {
+			if (edges[i][1] == 0)
+				max += edges[i][2];
+		}
+		lst.clear();
+		boolean visited2[] = new boolean[n];
+		List<Integer> NodeList1 = new ArrayList<>();
+		for (int i = 0; i < n; i++) {
+			NodeList1.add(i);
+			ConnectivityLst.add(i);
+		}
+		checkConnectivity(g, edges, visited2, 0, lst);
+		for (int i = 0; i < visited2.length; i++)
+			visited2[i] = false;
+		NodeList1.removeAll(ConnectivityLst);
+		System.out.println("Nodes connected with 's' : " + NodeList1);
+		System.out.println("Nodes connected with 't' : " + ConnectivityLst);
+
+		return max;
+	}
+
+	private static void checkConnectivity(graph g, int[][] edges, boolean[] visited2, int s, List<Integer> lst) {
+		// TODO Auto-generated method stub
+		visited2[s] = true;
+		boolean vTemp[] = new boolean[visited2.length];
+		List<Integer> lstTemp = new ArrayList<>();
+		for (int i = 0; i < visited2.length; i++) {
+			if (visited2[i])
+				vTemp[i] = true;
+			else
+				vTemp[i] = false;
+		}
+		lst.add(s);
+		ConnectivityLst.remove(new Integer(s));
+		lstTemp.addAll(lst);
+
+		for (int i : g.ll[s]) {
+			boolean f = false;
+			if (!visited2[i]) {
+				for (int j = 0; j < edges.length; j++) {
+					if (edges[j][0] == s && edges[j][1] == i && edges[j][2] > 0) {
+						f = true;
+					}
+				}
+				if (f) {
+					checkConnectivity(g, edges, vTemp, i, lstTemp);
+					for (int j = 0; j < visited2.length; j++) {
+						if (visited2[j])
+							vTemp[j] = true;
+						else
+							vTemp[j] = false;
+					}
+					lstTemp.clear();
+					lstTemp.addAll(lst);
+				}
+			}
+		}
+	}
+
+	private static void traverse(graph g, int[][] edges, List<Integer> lst, boolean[] visited, int s, int t) {
+		// TODO Auto-generated method stub
+		visited[s] = true;
+		boolean vTemp[] = new boolean[visited.length];
+		List<Integer> lstTemp = new ArrayList<>();
+		for (int i = 0; i < visited.length; i++) {
+			if (visited[i])
+				vTemp[i] = true;
+			else
+				vTemp[i] = false;
+		}
+		lst.add(s);
+		lstTemp.addAll(lst);
+		if (s == t) {
+			Flst.add(lstTemp);
+			maxFlow(g, edges, lstTemp);
+			return;
+		}
+		for (int i = 0; i < g.ll[s].size(); i++) {
+			int node = g.ll[s].get(i);
+			boolean f = false;
+			if (!visited[node] && Flst.isEmpty()) {
+				for (int j = 0; j < edges.length; j++) {
+					if (edges[j][0] == s && edges[j][1] == node && edges[j][2] > 0) {
+						f = true;
+					}
+				}
+				if (f) {
+					traverse(g, edges, lstTemp, vTemp, node, t);
+					for (int j = 0; j < visited.length; j++) {
+						if (visited[j])
+							vTemp[j] = true;
+						else
+							vTemp[j] = false;
+					}
+					lstTemp.clear();
+					lstTemp.addAll(lst);
+				}
+			}
+		}
+	}
+
+	private static void maxFlow(graph g, int[][] edges, List<Integer> lstTemp) {
+		// TODO Auto-generated method stub
+		int min = findMinWeight(edges, lstTemp);
+		generateFlowRecidual(g, edges, lstTemp, min);
+
+	}
+
+	private static void generateFlowRecidual(graph g, int[][] edges, List<Integer> lstTemp, int min) {
+		// TODO Auto-generated method stub
+		boolean flag = false;
+		boolean flag2 = false;
+		for (int i = 0; i < edges.length; i++) {
+			flag = false;
+			for (int j = 0; j < lstTemp.size() - 1; j++) {
+				if (lstTemp.get(j) == edges[i][0] && lstTemp.get(j + 1) == edges[i][1]) {
+					flag = true;
+					flag2 = false;
+					for (int k = 0; k < edges.length; k++) {
+						if (edges[k][1] == edges[i][0] && edges[k][0] == edges[i][1]) {
+							flag2 = true;
+							edges[k][2] += min;
+							break;
+						}
+					}
+					if (!flag2) {
+						int newEdges[][] = new int[edges.length + 1][3];
+						System.arraycopy(edges, 0, newEdges, 0, edges.length);
+						newEdges[newEdges.length - 1][0] = edges[i][1];
+						newEdges[newEdges.length - 1][1] = edges[i][0];
+						newEdges[newEdges.length - 1][2] = min;
+						edges = newEdges;
+						g.ll[edges[i][1]].add(edges[i][0]);
+					}
+					edges[i][2] -= min;
+					break;
+				}
+			}
+
+		}
+		arr = new int[edges.length][3];
+		arr = edges;
+	}
+
+	private static int findMinWeight(int[][] edges, List<Integer> lstTemp) {
+		// TODO Auto-generated method stub
+		int min = 0;
+		for (int i = 0; i < edges.length; i++) {
+			if (edges[i][0] == lstTemp.get(0) && edges[i][1] == lstTemp.get(1)) {
+				min = edges[i][2];
+				break;
+			}
+		}
+
+		for (int i = 0; i < lstTemp.size() - 1; i++) {
+			for (int j = 0; j < edges.length; j++) {
+				if (edges[j][0] == lstTemp.get(i) && edges[j][1] == lstTemp.get(i + 1)) {
+					if (edges[j][2] < min)
+						min = edges[j][2];
+					break;
+				}
+
+			}
+		}
+		return min;
+	}
+
+}
--- HackerRank/src/test.java
+++ HackerRank/src/test.java
@@ -1,26 +1,43 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		List<Integer> x = new ArrayList<>();
-		x.add(101);
-		x.add(2);
-		x.add(3);
-		x.add(4);
-		x.add(5);
-		x.add(6);
-		x.add(7);
-		x.add(8);
-		x.add(9);
-		x.add(10);
-		x.add(11);
-		System.out.println(Integer.parseInt(x.get(0).toString(), 2));
-		for (int i = 0; i < x.size(); i++) {
-			System.out.println(Integer.toBinaryString(x.get(i)));
-		}
+//		List<Integer> x = new ArrayList<>();
+//		x.add(101);
+//		x.add(2);
+//		x.add(3);
+//		x.add(4);
+//		x.add(5);
+//		x.add(6);
+//		x.add(7);
+//		x.add(8);
+//		x.add(9);
+//		x.add(10);
+//		x.add(11);
+//		System.out.println(Integer.parseInt(x.get(0).toString(), 2));
+//		for (int i = 0; i < x.size(); i++) {
+//			System.out.println(Integer.toBinaryString(x.get(i)));
+//		}
+//
+//		System.out.println(~x.get(1));
+
+		HashMap<Integer, Integer> map = new HashMap<>();
+		map.put(1, 2);
+		map.put(2, 3);
+		map.put(3, 4);
+		for (int i : map.keySet())
+			System.out.println(i + " : " + map.get(i));
+
+		int temp[] = { 1, 2, 3 };
+		List<Integer> templst = new ArrayList<>();
+		templst = Arrays.stream(temp).boxed().collect(Collectors.toList());
+		System.out.println(templst);
 	}
 
 }
